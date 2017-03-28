a6598351d076b933b45464095f54736aefa9b806
HughesPHS <Hughesphs@gmail.com>
2017-03-28 19:39:21 -0400

    Update question_4.py

--- quiz-questions/question_4.py
+++ quiz-questions/question_4.py
@@ -8,11 +8,12 @@ def question_4():
     Given the following two collections:
 
     a_tuple = (19, 33, 12)
-    a_list = range(0, 10)
+    a_list = range(0, 10) 0-9
 
     What's the final value of `result`?
 
     result = a_list[3**2 - 8] + a_list[-1] + a_tuple[2]
+                    1               9           12
     """
     # Return the correct value.
-    pass
+    return 22
